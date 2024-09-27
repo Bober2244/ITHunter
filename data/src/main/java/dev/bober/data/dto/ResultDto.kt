413fc0c44359868f3c160b95a064f9357d73aaae
bi_ba <sarutunan082@gmail.com>
2024-09-27 14:43:43 +0300

    Mapped ResultDto

--- data/src/main/java/dev/bober/data/dto/ResultDto.kt
+++ data/src/main/java/dev/bober/data/dto/ResultDto.kt
@@ -6,8 +6,12 @@ data class ResultDto(
     val offers: OffersDto,
     val vacancies: VacanciesDto,
 ) {
-    fun toModel() = ResultModel(
-        offers.toModel(),
-        vacancies.toModel()
-    )
+    fun toModel() = offers.toModel()?.let { offers ->
+        vacancies.toModel()?.let { vacancies ->
+            ResultModel(
+                offers = offers,
+                vacancies = vacancies
+            )
+        }
+    }
 }
