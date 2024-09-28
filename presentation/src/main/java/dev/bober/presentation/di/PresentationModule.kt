4ee2115cbca566adc1ac39b1711836a3935d1f80
bi_ba <sarutunan082@gmail.com>
2024-09-28 22:07:26 +0300

    Added all working screens

--- presentation/src/main/java/dev/bober/presentation/di/PresentationModule.kt
+++ presentation/src/main/java/dev/bober/presentation/di/PresentationModule.kt
@@ -1,7 +1,12 @@
 package dev.bober.presentation.di
 
+import dev.bober.presentation.favorite.FavoriteViewModel
+import dev.bober.presentation.search.SearchViewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val presentationModule = module {
+    viewModel { FavoriteViewModel() }
+    viewModel { SearchViewModel() }
 
 }
--- presentation/src/main/java/dev/bober/presentation/favorite/FavoriteScreen.kt
+++ presentation/src/main/java/dev/bober/presentation/favorite/FavoriteScreen.kt
@@ -6,13 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dev.bober.presentation.R
+import dev.bober.presentation.databinding.FavoriteScreenBinding
 
 class FavoriteScreen : Fragment(R.layout.favorite_screen) {
+
+    private var _binding: FavoriteScreenBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+
+        return binding.root
     }
 }
