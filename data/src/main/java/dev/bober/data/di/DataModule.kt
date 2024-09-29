0fb03dcf69d240fb08f24e9b7f6b1e736ed991f8
bi_ba <sarutunan082@gmail.com>
2024-09-29 10:20:25 +0300

    Added caching

--- data/src/main/java/dev/bober/data/di/DataModule.kt
+++ data/src/main/java/dev/bober/data/di/DataModule.kt
@@ -5,6 +5,9 @@ import com.google.gson.GsonBuilder
 import dev.bober.data.api.Api
 import dev.bober.data.repository.ResponseRepositoryImpl
 import dev.bober.domain.repository.ResponseRepository
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import org.koin.android.ext.koin.androidApplication
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -16,14 +19,20 @@ val dataModule = module {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create(get()))
+            .client(get())
             .build()
     }
-
     single {
         get<Retrofit>().create(Api::class.java)
     }
+    single {
+        OkHttpClient.Builder()
+            .cache(get())
+            .build()
+    }
 
     single { GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.IDENTITY).create() }
+    single { Cache(androidApplication().cacheDir, 10L * 1024 * 1024) }
 
     singleOf(::ResponseRepositoryImpl) { bind<ResponseRepository>() }
 }
